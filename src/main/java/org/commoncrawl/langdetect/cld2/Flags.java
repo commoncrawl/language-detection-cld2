296f71bfd17c27db166053bb911fcb06ca999528
Sebastian Nagel <sebastian@commoncrawl.org>
2023-03-22 11:39:03 +0100

    CLD2 Flags: copy/cite documentation from compact_lang_det.h

--- src/main/java/org/commoncrawl/langdetect/cld2/Flags.java
+++ src/main/java/org/commoncrawl/langdetect/cld2/Flags.java
@@ -19,25 +19,68 @@ package org.commoncrawl.langdetect.cld2;
 /** Public use flags, debug output controls */
 public class Flags {
 
-  /** Force Greek, etc. => quads */
+  /**
+   * Force Greek, etc. => quads
+   * 
+   * <blockquote>Normally, several languages are detected solely by their
+   * Unicode script. Combined with appropriate lookup tables, this flag forces
+   * them instead to be detected via quadgrams. This can be a useful refinement
+   * when looking for meaningful text in these languages, instead of just
+   * character sets. The default tables do not support this use.. [<a href=
+   * "https://github.com/CLD2Owners/cld2/blob/b56fa78a2fe44ac2851bae5bf4f4693a0644da7b/public/compact_lang_det.h#L356">compact_lang_det.h</a>]</blockquote>
+   */
   public static final int kCLDFlagScoreAsQuads = 0x0100;
 
-  /** Debug HTML => stderr */
+  /**
+   * Debug HTML => stderr
+   * 
+   * <blockquote>For each detection call, write an HTML file to stderr, showing
+   * the text chunks and their detected languages.. [<a href=
+   * "https://github.com/CLD2Owners/cld2/blob/b56fa78a2fe44ac2851bae5bf4f4693a0644da7b/public/compact_lang_det.h#L362">compact_lang_det.h</a>]</blockquote>
+   */
   public static final int kCLDFlagHtml = 0x0200;
 
-  /** &lt;cr&gt; per chunk if HTML */
+  /**
+   * &lt;cr&gt; per chunk if HTML
+   * 
+   * <blockquote>In that HTML file, force a new line for each chunk.. [<a href=
+   * "https://github.com/CLD2Owners/cld2/blob/b56fa78a2fe44ac2851bae5bf4f4693a0644da7b/public/compact_lang_det.h#L365">compact_lang_det.h</a>]</blockquote>
+   */
   public static final int kCLDFlagCr = 0x0400;
 
-  /** More debug HTML => stderr */
+  /**
+   * More debug HTML => stderr
+   * 
+   * <blockquote>In that HTML file, show every lookup entry.. [<a href=
+   * "https://github.com/CLD2Owners/cld2/blob/b56fa78a2fe44ac2851bae5bf4f4693a0644da7b/public/compact_lang_det.h#L367">compact_lang_det.h</a>]</blockquote>
+   */
   public static final int kCLDFlagVerbose = 0x0800;
 
-  /** Less debug HTML => stderr */
+  /**
+   * Less debug HTML => stderr
+   * 
+   * <blockquote>In that HTML file, suppress most of the output detail..
+   * [<a href=
+   * "https://github.com/CLD2Owners/cld2/blob/b56fa78a2fe44ac2851bae5bf4f4693a0644da7b/public/compact_lang_det.h#L369">compact_lang_det.h</a>]</blockquote>
+   */
   public static final int kCLDFlagQuiet = 0x1000;
 
-  /** Echo input => stderr */
+  /**
+   * Echo input => stderr
+   * 
+   * <blockquote>Echo every input buffer to stderr. [<a href=
+   * "https://github.com/CLD2Owners/cld2/blob/b56fa78a2fe44ac2851bae5bf4f4693a0644da7b/public/compact_lang_det.h#L371">compact_lang_det.h</a>]</blockquote>
+   */
   public static final int kCLDFlagEcho = 0x2000;
 
-  /** Give best-effort answer, even on short text */
+  /**
+   * Give best-effort answer, even on short text
+   * 
+   * <blockquote>Give best-effort answer, instead of UNKNOWN_LANGUAGE. May be
+   * useful for short text if the caller prefers an approximate answer over
+   * none. [<a href=
+   * "https://github.com/CLD2Owners/cld2/blob/b56fa78a2fe44ac2851bae5bf4f4693a0644da7b/public/compact_lang_det.h#L373">compact_lang_det.h</a>]</blockquote>
+   */
   public static final int kCLDFlagBestEffort = 0x4000;
 
   public static final Flags BEST_EFFORT = new Flags();
@@ -48,6 +91,11 @@ public class Flags {
   private int flags = 0;
 
   /**
+   * <blockquote>Give best-effort answer, instead of UNKNOWN_LANGUAGE. May be
+   * useful for short text if the caller prefers an approximate answer over
+   * none. [<a href=
+   * "https://github.com/CLD2Owners/cld2/blob/b56fa78a2fe44ac2851bae5bf4f4693a0644da7b/public/compact_lang_det.h#L373">compact_lang_det.h</a>]</blockquote>
+   * 
    * @param value
    *          if true, give &quot;best-effort answer, even on short text&quot;
    */
