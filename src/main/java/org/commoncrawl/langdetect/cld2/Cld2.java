4ef60a544f7a37043a21f02552857c44db9f18db
Luca Foppiano <luca@foppiano.org>
2026-02-24 00:34:12 +0100

    feat: add support for mac ARM

--- src/main/java/org/commoncrawl/langdetect/cld2/Cld2.java
+++ src/main/java/org/commoncrawl/langdetect/cld2/Cld2.java
@@ -127,7 +127,7 @@ public class Cld2 {
   public static Result detect(byte[] bytes, CLDHints hints, int flags,
       boolean isPlainText) {
     Result res = new Result();
-    int language = Cld2Library.INSTANCE._ZN4CLD224ExtDetectLanguageSummaryEPKcibPKNS_8CLDHintsEiPNS_8LanguageEPiPdPSt6vectorINS_11ResultChunkESaISA_EES7_Pb(
+    int language = Cld2Library.INSTANCE._ZN4CLD224ExtDetectLanguageSummaryEPKcibPKNS_8CLDHintsEiPNS_8LanguageEPiPdPNSt3__16vectorINS_11ResultChunkENS9_9allocatorISB_EEEES7_Pb(
         bytes,
         bytes.length,
         isPlainText,
